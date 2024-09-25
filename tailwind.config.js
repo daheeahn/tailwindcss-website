a3416abdecd6f7d4ea1d52ce34a1f1eb8b8ef485
daheeahn <deg9810@gmail.com>
2024-09-24 19:15:40 -0700

    setting tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#23A6F0",
+        "dark-background-color": "#252B42",
+      },
+    },
+  },
+  plugins: [],
+};
