9f0a3153d14cc1961c0edadd514f5327c1e0b6f5
daheeahn <deg9810@gmail.com>
2024-09-24 19:57:03 -0700

    add layout: industry

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "primary-color": "#23A6F0",
         "light-blue-color": "#8EC2F2",
         "dark-background-color": "#252B42",
+        "light-text-color": "#FFFFFF",
         "second-text-color": "#737373",
       },
     },
